490240e83992acfc9c71df6b2f3811c1caed2240
Anderson Reichardt <andersonia@yahoo.com.br>
2026-07-01 19:07:51 -0300

    Task completed by Anderson Reichardt

--- app/main.py
+++ app/main.py
@@ -1,8 +1,73 @@
 class Car:
-    # write your code here
-    pass
+    def __init__(self, comfort_class: int, clean_mark: int, brand: str) -> None:
+        self.comfort_class = comfort_class
+        self.clean_mark = clean_mark
+        self.brand = brand
 
 
 class CarWashStation:
-    # write your code here
-    pass
+    def __init__(
+        self,
+        distance_from_city_center: float,
+        clean_power: int,
+        average_rating: float,
+        count_of_ratings: int,
+    ) -> None:
+        self.distance_from_city_center = distance_from_city_center
+        self.clean_power = clean_power
+        self.average_rating = average_rating
+        self.count_of_ratings = count_of_ratings
+
+    def wash_single_car(self, car: Car) -> None:
+        if car.clean_mark < self.clean_power:
+            car.clean_mark = self.clean_power
+
+    def calculate_washing_price(self, car: Car) -> float:
+        return round(
+            (
+                car.comfort_class
+                * (self.clean_power - car.clean_mark)
+                * self.average_rating
+            )
+            / self.distance_from_city_center,
+            1,
+        )
+
+    def serve_cars(self, cars: list[Car]) -> float:
+        income = 0.0
+        for car in cars:
+            if car.clean_mark < self.clean_power:
+                income += self.calculate_washing_price(car)
+                self.wash_single_car(car)
+        return income
+
+    def rate_service(self, new_rating: int) -> None:
+        total_rating = self.average_rating * self.count_of_ratings + new_rating
+        self.count_of_ratings += 1
+        self.average_rating = round(total_rating / self.count_of_ratings, 1)
+
+
+bmw = Car(comfort_class=3, clean_mark=3, brand="BMW")
+audi = Car(comfort_class=4, clean_mark=2, brand="Audi")
+
+print(bmw.clean_mark)  # 3
+print(audi.clean_mark)  # 2
+
+wash_station = CarWashStation(
+    distance_from_city_center=6, clean_power=8, average_rating=3.9, count_of_ratings=11
+)
+
+income = wash_station.serve_cars([bmw, audi])
+
+print(income)  # 17.5
+
+print(bmw.clean_mark)  # 6
+print(audi.clean_mark)  # 6
+
+print(wash_station.average_rating)  # 3.9
+print(wash_station.count_of_ratings)  # 11
+
+wash_station.rate_service(5)
+
+print(wash_station.average_rating)  # 4.0
+print(wash_station.count_of_ratings)  # 12
